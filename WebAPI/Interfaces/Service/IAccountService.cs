1e2e40ffa1e603fa38f577b6f1eb432468620e52
Kousik <rajkousik20@gmail.com>
2024-07-11 12:45:40 +0530

    added interfaces

---
+++ WebAPI/Interfaces/Service/IAccountService.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI.Interfaces.Service
+{
+    public interface IAccountService
+    {
+        public Task<Account> AddAccount(Account account);
+        public Task<IEnumerable<Account>> DeleteAccount(int accountId);
+        public Task<Account> GetAccountById(int accountId);
+        public Task<Account> UpdateAccount(int accountId, Account account);
+        public Task<Account> GetAccount(int accountId);
+    }
+}
---
+++ WebAPI/Interfaces/Service/ITransactionService.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI.Interfaces.Service
+{
+    public interface ITransactionService
+    {
+        public Task<Transaction> AddTransaction(Transaction transaction);
+        public Task<IEnumerable<Transaction>> DeleteTransaction(int transactionId);
+        public Task<Transaction> GetTransactionById(int transactionId);
+        public Task<Transaction> UpdateTransaction(int transactionId, Transaction transaction);
+        public Task<Transaction> GetTransaction(int transactionId);
+    }
+}
